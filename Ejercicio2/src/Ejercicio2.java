2abbd91ddea0ece38506303a688338ed364bad17
pmherrar01 <120454320+pmherrar01@users.noreply.github.com>
2024-05-12 14:21:58 +0200

    ejercicio2 por terminar

---
+++ Ejercicio2/src/Ejercicio2.java
@@ -0,0 +1,11 @@
+import java.util.ArrayList;
+import java.util.Scanner;
+
+public class Ejercicio2 {
+
+	public static void main(String[] args) {
+		ArrayList<Integer> lista = new ArrayList<>(); 
+
+	}
+
+}
