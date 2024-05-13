8a1598b2542bcfb96b3fb5200b2587b273f8d0ab
pmherrar01 <pmherrerar01@educarex.es>
2024-05-13 18:55:45 +0200

    Ejercicico4 por terminar

---
+++ Ejercicio4/src/Ejercicio4.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+import java.util.ArrayList;
+
+public class Ejercicio4 {
+
+	public static void rellenarLista(ArrayList<Integer> lista) {
+		Scanner entrada = new Scanner(System.in);
+		
+	}
+	
+	public static void main(String[] args) {
+		ArrayList<Integer> lista1 = new ArrayList<>();
+		ArrayList<Integer> lista2 = new ArrayList<>();
+
+	}
+
+}
